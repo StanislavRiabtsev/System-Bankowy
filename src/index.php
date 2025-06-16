c59914d19816f1b440ce0484c5b98a49b8377c69
Stanislav <stasikrybcev@gmail.com>
2025-06-16 17:23:13 +0200

    Fixed bug

--- src/index.php
+++ src/index.php
@@ -193,7 +193,7 @@ try {
 
     <div id="root"></div>
 
-    <script type="module" src="/php/System-Bankowy/my-app/dist/assets/index-DeWhnzFy.js"></script>
+    <script type="module" src="index-DeWhnzFy.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
